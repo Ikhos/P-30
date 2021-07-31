6f0fc9074d359a72f204c649b66a723475e353ad
Ikhos <73320135+Ikhos@users.noreply.github.com>
2021-07-30 19:57:23 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -18,9 +18,21 @@ function setup() {
     myEngine = Engine.create();
     myWorld = myEngine.world;
     platformOne = new Ground(500, 400, 400, 20);
-    boxOne = new Box(500, 330, 40, 40);
-    boxTwo = new Box(480, 370, 40, 40);
-    boxThree = new Box(520, 370, 40, 40);
+    //set 1
+    boxOne = new Box(420, 280, 40, 40);
+    boxTwo = new Box(400, 320, 40, 40);
+    boxThree = new Box(440, 320, 40, 40);
+    boxFour = new Box(420, 360, 40, 40);
+    boxFive = new Box(380, 360, 40, 40);
+    boxSix = new Box(460, 360, 40, 40);
+    hexagon = new Hexagon(150, 250);
+    //set 2
+    boxOne2 = new Box(580, 280, 40, 40);
+    boxTwo2 = new Box(560, 320, 40, 40);
+    boxThree2 = new Box(600, 320, 40, 40);
+    boxFour2 = new Box(580, 360, 40, 40);
+    boxFive2 = new Box(540, 360, 40, 40);
+    boxSix2 = new Box(640, 360, 40, 40);
     hexagon = new Hexagon(150, 250);
     sling = new Slingshot(hexagon.body, {x: 150, y:250});
 };
@@ -36,6 +48,15 @@ function draw() {
     boxOne.display();
     boxTwo.display();
     boxThree.display();
+    boxFour.display();
+    boxFive.display();
+    boxSix.display();
+    boxOne2.display();
+    boxTwo2.display();
+    boxThree2.display();
+    boxFour2.display();
+    boxFive2.display();
+    boxSix2.display();
 };
 
 function mouseDragged() {
